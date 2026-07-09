be50c6091f321385ee63416334c6cc2a451d39f4
YannikKrantz <yannikkrantz@proton.me>
2026-07-09 15:46:07 +0200

    feat(main): implement uvicorn server setup and update dependencies

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import uvicorn
+
+from src.config import settings
 from src.logging_config import setup_logging
 
 setup_logging()
 
-def main():
-    print("Hello from cc-weather-api!")
-
+app = "src.app:app"
+def main() -> None:
+    uvicorn.run(
+        app,
+        host=settings.app_host,
+        port=settings.app_port,
+        reload=settings.reload,
+    )
 
 if __name__ == "__main__":
     main()
