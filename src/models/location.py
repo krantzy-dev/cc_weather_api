33842f71647b08888a052ffbab0df8b4d5565004
YannikKrantz <yannikkrantz@proton.me>
2026-07-11 13:40:12 +0200

    fix(DB): updated location.py

--- src/models/location.py
+++ src/models/location.py
@@ -6,10 +6,11 @@ from sqlalchemy.orm import Mapped, mapped_column
 from src.models.base import Base
 
 
-class User(Base):
-    __tablename__ = "user"
+class Location(Base):
+    __tablename__ = "location"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(unique=True, index=True)
-    password_hash: Mapped[str]
+    name: Mapped[str]
+    lat: Mapped[float]
+    lon: Mapped[float]
     created: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
