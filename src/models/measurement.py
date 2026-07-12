84f403c15abfb5f43b3a25e3e004e31823f76787
YannikKrantz <yannikkrantz@proton.me>
2026-07-12 14:55:38 +0200

    feat(measurement): add cascade delete to foreign keys for location and metric

--- src/models/measurement.py
+++ src/models/measurement.py
@@ -11,8 +11,10 @@ class Measurement(Base):
     __tablename__ = "measurement"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    location_id: Mapped[int] = mapped_column(ForeignKey("location.id"), index=True)
-    metric_id: Mapped[int] = mapped_column(ForeignKey("metric.id"), index=True)
+    location_id: Mapped[int] = mapped_column(
+        ForeignKey("location.id", ondelete="CASCADE"), index=True
+    )
+    metric_id: Mapped[int] = mapped_column(ForeignKey("metric.id", ondelete="CASCADE"), index=True)
     ts_value: Mapped[datetime] = mapped_column(DateTime(timezone=True), index=True)
     ts_created: Mapped[datetime] = mapped_column(DateTime(timezone=True))
     forecast_horizon: Mapped[int]
